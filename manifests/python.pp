f0a68b6f6405d215d40b60786f840ae90b0cfd3c
Richard Pijnenburg <richard@ispavailability.com>
2013-08-30 09:26:17 +0200

    Make puppet-lint happy

--- manifests/python.pp
+++ manifests/python.pp
@@ -1,26 +1,26 @@
 # there are many python bindings for elasticsearch. This provides all
 # the ones we know about http://www.elasticsearch.org/guide/clients/
 define elasticsearch::python (
-  $ensure   = "installed",
+  $ensure   = 'installed',
 ) {
 
   # make sure the package name is valid and setup the provider as
   # necessary
   case $name {
-    "pyes": {
-      $provider = "pip"
+    'pyes': {
+      $provider = 'pip'
     }
-    "rawes": {
-      $provider = "pip"
+    'rawes': {
+      $provider = 'pip'
     }
-    "pyelasticsearch": {
-      $provider = "pip"
+    'pyelasticsearch': {
+      $provider = 'pip'
     }
-    "ESClient": {
-      $provider = "pip"
+    'ESClient': {
+      $provider = 'pip'
     }
-    "elasticutils": {
-      $provider = "pip"
+    'elasticutils': {
+      $provider = 'pip'
     }
     default: {
       fail("unknown python binding package '${name}'")
@@ -28,7 +28,7 @@ define elasticsearch::python (
   }
 
   package { $name:
-    ensure => $ensure,
+    ensure   => $ensure,
     provider => $provider,
   }
 }
