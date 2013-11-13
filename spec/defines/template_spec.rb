aea2ec5d8659611f90494cd2e2ebe97e7bf44a63
Richard Pijnenburg <richard@ispavailability.com>
2013-11-13 16:11:06 +0100

    Fix template spec tests

--- spec/defines/template_spec.rb
+++ spec/defines/template_spec.rb
@@ -9,58 +9,35 @@ describe 'elasticsearch::template', :type => 'define' do
   context "Add a template" do
 
     let :params do {
-      :file => 'puppet:///path/to/foo.json',
+      :ensure => 'present',
+      :file   => 'puppet:///path/to/foo.json',
     } end
 
-    it { should contain_file('/etc/elasticsearch/templates_import/elasticsearch-template-foo.json').with(:source => 'puppet:///path/to/foo.json') }
-    it { should contain_exec('insert_template foo').with(:command => 'curl -s -XPUT http://localhost:9200/_template/foo -d @/etc/elasticsearch/templates_import/elasticsearch-template-foo.json', :unless => 'test $(curl -s \'http://localhost:9200/_template/foo?pretty=true\' | wc -l) -gt 1') }
+    it { should contain_file('/etc/elasticsearch/templates_import/elasticsearch-template-foo.json').with(:source => 'puppet:///path/to/foo.json', :notify => "Exec[delete_template_foo]", :require => "Exec[mkdir_templates]") }
+    it { should contain_exec('insert_template_foo').with(:command => 'curl -s -XPUT http://localhost:9200/_template/foo -d @/etc/elasticsearch/templates_import/elasticsearch-template-foo.json', :unless => 'test $(curl -s \'http://localhost:9200/_template/foo?pretty=true\' | wc -l) -gt 1') }
   end
 
   context "Delete a template" do
 
     let :params do {
-      :delete => true
+      :ensure => 'absent'
     } end
 
     it { should_not contain_file('/etc/elasticsearch/templates_import/elasticsearch-template-foo.json').with(:source => 'puppet:///path/to/foo.json') }
-    it { should_not contain_exec('insert_template foo') }
-    it { should contain_exec('delete_template foo').with(:command => 'curl -s -XDELETE http://localhost:9200/_template/foo', :notify => nil, :onlyif => 'test $(curl -s \'http://localhost:9200/_template/foo?pretty=true\' | wc -l) -gt 1' ) }
+    it { should_not contain_exec('insert_template_foo') }
+    it { should contain_exec('delete_template_foo').with(:command => 'curl -s -XDELETE http://localhost:9200/_template/foo', :notify => nil, :onlyif => 'test $(curl -s \'http://localhost:9200/_template/foo?pretty=true\' | wc -l) -gt 1' ) }
   end
 
-  context "Replace a template" do
-
-    let :params do {
-      :replace => true,
-      :file => 'puppet:///path/to/foo.json'
-    } end
-
-    it { should contain_file('/etc/elasticsearch/templates_import/elasticsearch-template-foo.json').with(:source => 'puppet:///path/to/foo.json') }
-    it { should contain_exec('insert_template foo').with(:command => 'curl -s -XPUT http://localhost:9200/_template/foo -d @/etc/elasticsearch/templates_import/elasticsearch-template-foo.json', :unless => 'test $(curl -s \'http://localhost:9200/_template/foo?pretty=true\' | wc -l) -gt 1') }
-    it { should contain_exec('delete_template foo').with(:command => 'curl -s -XDELETE http://localhost:9200/_template/foo', :notify => 'Exec[insert_template foo]', :onlyif => 'test $(curl -s \'http://localhost:9200/_template/foo?pretty=true\' | wc -l) -gt 1' ) }
-
-  end
-
-  context "Try replace and delete at the same time" do
-
-    let :params do {
-      :replace => true,
-      :delete => true,
-      :file => 'puppet:///path/to/foo.json'
-    } end
-
-    it { expect { should raise_error(Puppet::Error) } }
-
-  end
-
-  context "Add template with alternative host" do
+  context "Add template with alternative host and port" do
 
     let :params do {
       :file => 'puppet:///path/to/foo.json',
-      :host => 'otherhost'
+      :host => 'otherhost',
+      :port => '9201'
     } end
 
     it { should contain_file('/etc/elasticsearch/templates_import/elasticsearch-template-foo.json').with(:source => 'puppet:///path/to/foo.json') }
-    it { should contain_exec('insert_template foo').with(:command => 'curl -s -XPUT http://otherhost:9200/_template/foo -d @/etc/elasticsearch/templates_import/elasticsearch-template-foo.json', :unless => 'test $(curl -s \'http://otherhost:9200/_template/foo?pretty=true\' | wc -l) -gt 1') }
+    it { should contain_exec('insert_template_foo').with(:command => 'curl -s -XPUT http://otherhost:9201/_template/foo -d @/etc/elasticsearch/templates_import/elasticsearch-template-foo.json', :unless => 'test $(curl -s \'http://otherhost:9201/_template/foo?pretty=true\' | wc -l) -gt 1') }
   end
 
 end
