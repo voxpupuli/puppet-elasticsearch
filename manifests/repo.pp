e87df92b5693b78adfcc83402fbeab6cdcb5eee4
Richard Pijnenburg <richard@ispavailability.com>
2014-02-06 13:14:37 +0100

    Set key location to a http file instead of key server

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -36,7 +36,7 @@ class elasticsearch::repo {
         release     => 'stable',
         repos       => 'main',
         key         => 'D88E42B4',
-        key_server  => 'pgp.mit.edu',
+        key_source  => 'http://packages.elasticsearch.org/GPG-KEY-elasticsearch',
         include_src => false,
       }
     }
