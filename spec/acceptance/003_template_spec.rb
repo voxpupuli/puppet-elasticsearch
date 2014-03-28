55b6ae8db20852735c20952170c6b1f7ae307ed5
Richard Pijnenburg <richard@ispavailability.com>
2014-03-28 14:25:08 +0100

    Improve tests
    Add random cluster name to avoid weird issues
    Add some more basic tests to ensure it all works.

--- spec/acceptance/003_template_spec.rb
+++ spec/acceptance/003_template_spec.rb
@@ -89,6 +89,9 @@ bad_json='{
 }
 '
 
+
+cluster_name = SecureRandom.hex(10)
+
 describe "elasticsearch template define:" do
 
   shell("mkdir -p #{default['distmoduledir']}/another/files")
@@ -98,7 +101,7 @@ describe "elasticsearch template define:" do
   describe "Insert a template with valid json content" do
 
     it 'should run successfully' do
-      pp = "class { 'elasticsearch': config => { 'node.name' => 'elasticsearch001' }, manage_repo => true, repo_version => '1.0', java_install => true }
+			pp = "class { 'elasticsearch': config => { 'node.name' => 'elasticsearch001', 'cluster.name' => '#{cluster_name}' }, manage_repo => true, repo_version => '1.0', java_install => true }
           elasticsearch::template { 'foo': ensure => 'present', file => 'puppet:///modules/another/good.json' }"
 
       # Run it twice and test for idempotency
@@ -115,7 +118,7 @@ describe "elasticsearch template define:" do
     describe "Insert a template with bad json content" do
 
       it 'run should fail' do
-        pp = "class { 'elasticsearch': config => { 'node.name' => 'elasticsearch001' }, manage_repo => true, repo_version => '1.0', java_install => true }
+				pp = "class { 'elasticsearch': config => { 'node.name' => 'elasticsearch001', 'cluster.name' => '#{cluster_name}' }, manage_repo => true, repo_version => '1.0', java_install => true }
              elasticsearch::template { 'foo': ensure => 'present', file => 'puppet:///modules/another/bad.json' }"
 
         apply_manifest(pp, :expect_failures => true)
--- spec/acceptance/011_service_spec.rb
+++ spec/acceptance/011_service_spec.rb
@@ -2,11 +2,19 @@ require 'spec_helper_acceptance'
 
 describe "Service tests:" do
 
+  cluster_name = SecureRandom.hex(10)
+
   case fact('osfamily')
     when 'RedHat'
       defaults_file = '/etc/sysconfig/elasticsearch'
+			service_name  = 'elasticsearch'
+			package_name  = 'elasticsearch'
+			pid_file      = '/var/run/elasticsearch/elasticsearch.pid'
     when 'Debian'
       defaults_file = '/etc/default/elasticsearch'
+			service_name  = 'elasticsearch'
+			package_name  = 'elasticsearch'
+			pid_file      = '/var/run/elasticsearch.pid'
     when 'Suse'
       defaults_file = '/etc/sysconfig/elasticsearch'
   end
@@ -16,13 +24,45 @@ describe "Service tests:" do
 
     context "Change the defaults file" do
       it 'should run successfully' do
-        pp = "class { 'elasticsearch': manage_repo => true, repo_version => '1.0', java_install => true, config => { 'node.name' => 'elasticsearch001' }, init_defaults => { 'ES_USER' => 'root' } }"
+				pp = "class { 'elasticsearch': manage_repo => true, repo_version => '1.0', java_install => true, config => { 'node.name' => 'elasticsearch001', 'cluster.name' => '#{cluster_name}' }, init_defaults => { 'ES_USER' => 'root' } }"
 
         # Run it twice and test for idempotency
         apply_manifest(pp, :catch_failures => true)
         sleep 10
         expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero
       end
+    end
+
+    if fact('osfamily') != 'Suse'
+      describe service(service_name) do
+        it { should be_enabled }
+        it { should be_running } 
+      end
+
+      describe package(package_name) do
+        it { should be_installed }
+      end
+
+			describe file(pid_file) do
+	      it { should be_file }
+				its(:content) { should match /[0-9]+/ }
+			end
+    end
+
+    describe port(9200) do
+      it {
+        sleep 10
+        should be_listening
+      }
+    end
+
+    describe file('/etc/elasticsearch/elasticsearch.yml') do
+      it { should be_file }
+      it { should contain 'name: elasticsearch001' }
+    end
+
+    describe file('/etc/elasticsearch/templates_import') do
+      it { should be_directory }
     end
 
     context "Make sure we have ES_USER=root" do
