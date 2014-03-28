350632226d1dbf1ed2899d5cb58b853992cae991
Richard Pijnenburg <richard@ispavailability.com>
2014-03-28 14:20:15 +0100

    Improve spec helper
    - Add puppet dependencies via scp to avoid downloading them all the time
    - Moved all dependencies for augeas to node files so remove them here.

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,5 +1,6 @@
 require 'beaker-rspec'
 require 'pry'
+require 'securerandom'
 
 hosts.each do |host|
   # Install Puppet
@@ -10,19 +11,6 @@ hosts.each do |host|
     install_package host, 'rubygems'
     on host, "gem install puppet --no-ri --no-rdoc --version '~> #{puppetversion}'"
     on host, "mkdir -p #{host['distmoduledir']}"
-
-    if fact('osfamily') == 'Debian'
-      install_package host, 'pkg-config ruby1.8-dev libaugeas-dev'
-    end
-    if fact('osfamily') == 'RedHat'
-      install_package host, 'ruby-devel augeas-devel'
-    end
-    if fact('osfamily') == 'Suse'
-      install_package host, 'ruby-devel augeas-devel libxml2-devel'
-    end
-
-    on host, "gem install ruby-augeas --no-ri --no-rdoc"
-
   end
 end
 
@@ -40,14 +28,17 @@ RSpec.configure do |c|
     hosts.each do |host|
 
       if !host.is_pe?
-        on host, puppet('module','install','puppetlabs-stdlib', '-v 3.2.0'), { :acceptable_exit_codes => [0,1] }
+				scp_to(host, '/home/jenkins/puppet/puppetlabs-stdlib-3.2.0.tar.gz', '/tmp/puppetlabs-stdlib-3.2.0.tar.gz')
+				on host, puppet('module','install','/tmp/puppetlabs-stdlib-3.2.0.tar.gz'), { :acceptable_exit_codes => [0,1] }
       end
       if fact('osfamily') == 'Debian'
-        on host, puppet('module','install','puppetlabs-apt'), { :acceptable_exit_codes => [0,1] }
+				scp_to(host, '/home/jenkins/puppet/puppetlabs-apt-1.4.2.tar.gz', '/tmp/puppetlabs-apt-1.4.2.tar.gz')
+				on host, puppet('module','install','/tmp/puppetlabs-apt-1.4.2.tar.gz'), { :acceptable_exit_codes => [0,1] }
       end
       if fact('osfamily') == 'Suse'
         on host, puppet('module','install','darin-zypprepo'), { :acceptable_exit_codes => [0,1] }
       end
+
     end
   end
 end
