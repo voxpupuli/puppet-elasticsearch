76f1cb4432a8279cd17932d5123d22f6a6c4b75c
Richard Pijnenburg <richard@ispavailability.com>
2014-04-03 14:18:32 +0200

    Add system test to verify we manage the rights of the plugin dir correctly

--- spec/acceptance/004_plugin_spec.rb
+++ spec/acceptance/004_plugin_spec.rb
@@ -97,4 +97,51 @@ describe "elasticsearch plugin define:" do
   end
 
 
+  describe "install plugin while running ES under user 'root'" do
+
+    it 'should run successfully' do
+			pp = "class { 'elasticsearch': config => { 'node.name' => 'elasticsearch001', 'cluster.name' => '#{cluster_name}' }, manage_repo => true, repo_version => '1.0', java_install => true, elasticsearch_user => 'root', elasticsearch_group => 'root' }
+            elasticsearch::plugin{'lmenezes/elasticsearch-kopf': module_dir => 'kopf' }
+           "
+
+      # Run it twice and test for idempotency
+      apply_manifest(pp, :catch_failures => true)
+			sleep 5
+      expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero
+    end
+
+    if fact('osfamily') != 'Suse'
+      describe service(service_name) do
+        it { should be_enabled }
+        it { should be_running } 
+      end
+
+      describe package(package_name) do
+        it { should be_installed }
+      end
+
+			describe file(pid_file) do
+	      it { should be_file }
+				its(:content) { should match /[0-9]+/ }
+			end
+    end
+
+    describe port(9200) do
+      it {
+        sleep 10
+        should be_listening
+      }
+    end
+
+    it 'make sure the directory exists' do
+      shell('ls /usr/share/elasticsearch/plugins/kopf/', {:acceptable_exit_codes => 0})
+    end
+
+    it 'make sure elasticsearch reports it as existing' do
+      sleep 10
+      shell("/usr/bin/curl http://localhost:9200/_nodes/?plugin | grep kopf", {:acceptable_exit_codes => 0})
+    end
+
+	end
+
 end
