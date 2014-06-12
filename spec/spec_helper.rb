2ed03480f0d6786da76345ee5e615a97dc63dc15
Richard Pijnenburg <richard@ispavailability.com>
2014-06-12 14:10:01 +0200

    [TESTING] Add rspec randomization to travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,3 @@
-# Temporary due to https://github.com/rspec/rspec-expectations/issues/573
-module Encoding; CompatibilityError = Class.new(StandardError); end if RUBY_VERSION < '1.9'
-
 require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
 
