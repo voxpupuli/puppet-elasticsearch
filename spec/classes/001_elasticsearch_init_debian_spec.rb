d0ba9239ae49b19c8b7e95f5376a5820d251f4a3
Richard Pijnenburg <richard@ispavailability.com>
2014-06-13 14:06:09 +0200

    [TESTING] update tests to correct for package removal

--- spec/classes/001_elasticsearch_init_debian_spec.rb
+++ spec/classes/001_elasticsearch_init_debian_spec.rb
@@ -179,7 +179,7 @@ describe 'elasticsearch', :type => 'class' do
           })
         }
 
-        it { should contain_package('elasticsearch').with(:ensure => 'purged') }
+        it { should contain_package('elasticsearch').with(:ensure => 'absent') }
 
       end
 
