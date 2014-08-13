18e603e37ebede9091dba46f1bed93f43041ce83
Richard Pijnenburg <richard@ispavailability.com>
2014-08-13 12:40:23 +0000

    [TESTING] Make ordering optional
    Ordering option was available 3.3 and up

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,6 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |c|
   Puppet.settings[:strict_variables]=true if ENV['STRICT_VARIABLES'] == 'true'
-  Puppet.settings[:ordering]='random'
+  Puppet.settings[:ordering]='random' if ENV['ORDERING_RANDOM'] == 'true'
   c.parser = 'future' if ENV['FUTURE_PARSER'] == 'true'
 end
