2b106432393d317fe0cd2acff9a7e03f1abd92fa
Richard Pijnenburg <richard@ispavailability.com>
2014-08-13 12:08:40 +0000

    [TESTING] Improve spec testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,5 +2,7 @@ require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |c|
+  Puppet.settings[:strict_variables]=true
+  Puppet.settings[:ordering]='random'
   c.parser = 'future' if ENV['FUTURE_PARSER'] == 'true'
 end
