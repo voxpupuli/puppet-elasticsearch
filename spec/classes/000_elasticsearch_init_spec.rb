0e12b4ae137c6c386eeebfd51d93f4f274228e84
Richard Pijnenburg <richard@ispavailability.com>
2015-01-08 21:59:35 +0000

    Fix spec tests

--- spec/classes/000_elasticsearch_init_spec.rb
+++ spec/classes/000_elasticsearch_init_spec.rb
@@ -242,7 +242,8 @@ describe 'elasticsearch', :type => 'class' do
         it { should contain_file('/var/log/elasticsearch').with(:owner => 'myesuser', :group => 'myesgroup') }
         it { should contain_file('/usr/share/elasticsearch').with(:owner => 'myesuser', :group => 'myesgroup') }
         it { should contain_file('/usr/share/elasticsearch/plugins').with(:owner => 'myesuser', :group => 'myesgroup') }
-        it { should contain_file('/var/run/elasticsearch').with(:owner => 'myesuser', :group => 'myesgroup') } if facts[:osfamily] == 'RedHat'
+        it { should contain_file('/usr/share/elasticsearch/data').with(:owner => 'myesuser', :group => 'myesgroup') }
+        it { should contain_file('/var/run/elasticsearch').with(:owner => 'myesuser', :group => 'myesuser') } if facts[:osfamily] == 'RedHat'
 
       end
 
