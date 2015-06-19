cbcb8af61310c2886f76a5af33961ce127b7276d
Richard Pijnenburg <richard@ispavailability.com>
2015-06-19 11:43:51 +0200

    Fix OpenSuse 13.1 testing

--- spec/acceptance/016_package_pin_spec.rb
+++ spec/acceptance/016_package_pin_spec.rb
@@ -2,103 +2,106 @@ require 'spec_helper_acceptance'
 
 describe "Package pinning:" do
 
-  describe "Pinning enabled" do
+  if fact('osfamily') != 'Suse'
 
-    describe "Setup" do
+    describe "Pinning enabled" do
 
-      it 'should run successful' do
-        write_hiera_config('')
-        pp = "class { 'elasticsearch': config => { 'cluster.name' => '#{test_settings['cluster_name']}'}, manage_repo => true, repo_version => '#{test_settings['repo_version']}', version => '#{test_settings['install_package_version']}', java_install => true }
-              elasticsearch::instance { 'es-01': config => { 'node.name' => 'elasticsearch001', 'http.port' => '#{test_settings['port_a']}' } }
-             "
-        # Run it twice and test for idempotency
-        apply_manifest(pp, :catch_failures => true)
-        expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero
-      end
+      describe "Setup" do
 
-      describe package(test_settings['package_name']) do
-        it { should be_installed.with_version(test_settings['install_version']) }
-      end
+        it 'should run successful' do
+          write_hiera_config('')
+          pp = "class { 'elasticsearch': config => { 'cluster.name' => '#{test_settings['cluster_name']}'}, manage_repo => true, repo_version => '#{test_settings['repo_version']}', version => '#{test_settings['install_package_version']}', java_install => true }
+                elasticsearch::instance { 'es-01': config => { 'node.name' => 'elasticsearch001', 'http.port' => '#{test_settings['port_a']}' } }
+               "
+          # Run it twice and test for idempotency
+          apply_manifest(pp, :catch_failures => true)
+          expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero
+        end
 
-    end # end setup
+        describe package(test_settings['package_name']) do
+          it { should be_installed.with_version(test_settings['install_version']) }
+        end
 
-    describe "Run upgrade" do
-      it 'should run fine' do
-        case fact('osfamily')
-        when 'Debian'
-          shell('apt-get update && apt-get -y install elasticsearch')
-        when 'RedHat'
-          shell('yum -y update elasticsearch')
+      end # end setup
+
+      describe "Run upgrade" do
+        it 'should run fine' do
+          case fact('osfamily')
+          when 'Debian'
+            shell('apt-get update && apt-get -y install elasticsearch')
+          when 'RedHat'
+            shell('yum -y update elasticsearch')
+          end
         end
       end
-    end
 
-    describe "check installed package" do
+      describe "check installed package" do
 
-      describe package(test_settings['package_name']) do
-        it { should be_installed.with_version(test_settings['install_version']) }
+        describe package(test_settings['package_name']) do
+          it { should be_installed.with_version(test_settings['install_version']) }
+        end
+
       end
 
-    end
+      describe "Upgrade" do
 
-    describe "Upgrade" do
+        it 'should run successful' do
+          pp = "class { 'elasticsearch': config => { 'cluster.name' => '#{test_settings['cluster_name']}'}, manage_repo => true, repo_version => '#{test_settings['repo_version']}', version => '#{test_settings['upgrade_package_version']}', java_install => true }
+                elasticsearch::instance { 'es-01': config => { 'node.name' => 'elasticsearch001', 'http.port' => '#{test_settings['port_a']}' } }
+               "
+          # Run it twice and test for idempotency
+          apply_manifest(pp, :catch_failures => true)
+          expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero
+        end
 
-      it 'should run successful' do
-        pp = "class { 'elasticsearch': config => { 'cluster.name' => '#{test_settings['cluster_name']}'}, manage_repo => true, repo_version => '#{test_settings['repo_version']}', version => '#{test_settings['upgrade_package_version']}', java_install => true }
-              elasticsearch::instance { 'es-01': config => { 'node.name' => 'elasticsearch001', 'http.port' => '#{test_settings['port_a']}' } }
-             "
-        # Run it twice and test for idempotency
-        apply_manifest(pp, :catch_failures => true)
-        expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero
-      end
+        describe package(test_settings['package_name']) do
+          it { should be_installed.with_version(test_settings['upgrade_version']) }
+        end
 
-      describe package(test_settings['package_name']) do
-        it { should be_installed.with_version(test_settings['upgrade_version']) }
-      end
+      end # end setup
 
-    end # end setup
-
-    describe "Run upgrade" do
-      it 'should run fine' do
-        case fact('osfamily')
-        when 'Debian'
-          shell('apt-get update && apt-get -y install elasticsearch')
-        when 'RedHat'
-          shell('yum -y update elasticsearch')
+      describe "Run upgrade" do
+        it 'should run fine' do
+          case fact('osfamily')
+          when 'Debian'
+            shell('apt-get update && apt-get -y install elasticsearch')
+          when 'RedHat'
+            shell('yum -y update elasticsearch')
+          end
         end
       end
+
+      describe "check installed package" do
+
+        describe package(test_settings['package_name']) do
+          it { should be_installed.with_version(test_settings['upgrade_version']) }
+        end
+
+      end
+
     end
 
-    describe "check installed package" do
+    describe "Cleanup" do
 
-      describe package(test_settings['package_name']) do
-        it { should be_installed.with_version(test_settings['upgrade_version']) }
+      it 'should run successfully' do
+        pp = "class { 'elasticsearch': ensure => 'absent' }
+              elasticsearch::instance{ 'es-01': ensure => 'absent' }
+             "
+
+        apply_manifest(pp, :catch_failures => true)
+      end
+
+      describe file('/etc/elasticsearch/es-01') do
+        it { should_not be_directory }
+      end
+
+      describe service(test_settings['service_name_a']) do
+        it { should_not be_enabled }
+        it { should_not be_running }
       end
 
     end
 
   end
-
-  describe "Cleanup" do
-
-    it 'should run successfully' do
-      pp = "class { 'elasticsearch': ensure => 'absent' }
-            elasticsearch::instance{ 'es-01': ensure => 'absent' }
-           "
-
-      apply_manifest(pp, :catch_failures => true)
-    end
-
-    describe file('/etc/elasticsearch/es-01') do
-      it { should_not be_directory }
-    end
-
-    describe service(test_settings['service_name_a']) do
-      it { should_not be_enabled }
-      it { should_not be_running }
-    end
-
-  end
-
 
 end
