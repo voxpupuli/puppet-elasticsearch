bcffd18d4097c53b1e0872d0251a8b4bffefa609
Richard Pijnenburg <richard@ispavailability.com>
2015-08-20 11:42:53 +0200

    Make selecting the package for integation testing easier

--- spec/spec_acceptance_integration.rb
+++ spec/spec_acceptance_integration.rb
@@ -3,23 +3,19 @@ def get_url
 
   urls = { 
     'URL_MASTER' => 'http://s3-eu-west-1.amazonaws.com/build-eu.elasticsearch.org/origin/master/nightly/JDK7/elasticsearch-latest-SNAPSHOT.$EXT$',
-    'URL_17' => 'http://s3-eu-west-1.amazonaws.com/build.eu-west-1.elastic.co/origin/1.7/nightly/JDK7/elasticsearch-latest-SNAPSHOT.$EXT$',
-    'URL_16' => 'http://s3-eu-west-1.amazonaws.com/build.eu-west-1.elastic.co/origin/1.6/nightly/JDK7/elasticsearch-latest-SNAPSHOT.$EXT$',
-    'URL_15' => 'http://s3-eu-west-1.amazonaws.com/build.eu-west-1.elastic.co/origin/1.5/nightly/JDK7/elasticsearch-latest-SNAPSHOT.$EXT$',
-    'URL_14' => 'http://s3-eu-west-1.amazonaws.com/build-eu.elasticsearch.org/origin/1.4/nightly/JDK7/elasticsearch-latest-SNAPSHOT.$EXT$',
-    'URL_13' => 'http://s3-eu-west-1.amazonaws.com/build-eu.elasticsearch.org/origin/1.3/nightly/JDK7/elasticsearch-latest-SNAPSHOT.$EXT$',
-    'URL_12' => 'http://s3-eu-west-1.amazonaws.com/build-eu.elasticsearch.org/origin/1.2/nightly/JDK7/elasticsearch-latest-SNAPSHOT.$EXT$',
-    'URL_11' => 'http://s3-eu-west-1.amazonaws.com/build-eu.elasticsearch.org/origin/1.1/nightly/JDK7/elasticsearch-latest-SNAPSHOT.$EXT$',
-    'URL_10' => 'http://s3-us-west-2.amazonaws.com/build.elasticsearch.org/origin/1.0/nightly/JDK6/elasticsearch-latest-SNAPSHOT.$EXT$',
+    'URL_SNAP' => 'http://s3-eu-west-1.amazonaws.com/build.eu-west-1.elastic.co/origin/$VERSION$/nightly/JDK7/elasticsearch-latest-SNAPSHOT.$EXT$',
     'URL_TAGS' => 'http://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-$VERSION$.$EXT$'
   }
 
   es_version = ENV['ES_VERSION']
 
-  if es_version =~ /_nightly$/
-    # We are testing a snapshot of a branch
-    ver = es_version.split('_')[0].tr('.', '')
-    url = urls["URL_#{ver}"]
+  if es_version == 'MASTER_nightly'
+    # We are testing the master branch snapshot
+    url = urls['URL_MASTER']
+  elsif es_version =~ /_nightly$/
+    # We are testing a version snapshot
+    ver = es_version.split('_')[0]
+    url = urls["URL_SNAP"].gsub('$VERSION$', ver)
   else
     # we are testing a released version
     url = urls["URL_TAGS"].gsub('$VERSION$', es_version)
