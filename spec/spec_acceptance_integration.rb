76ccf5de76ed495a03fd39a6d35a19343b4bf44c
Richard Pijnenburg <richard@ispavailability.com>
2015-11-04 17:07:29 +0100

    Add new url for all 2.x stable releases

--- spec/spec_acceptance_integration.rb
+++ spec/spec_acceptance_integration.rb
@@ -4,7 +4,8 @@ def get_url
   urls = { 
     'URL_MASTER' => 'http://s3-eu-west-1.amazonaws.com/build-eu.elasticsearch.org/origin/master/nightly/JDK7/elasticsearch-latest-SNAPSHOT.$EXT$',
     'URL_SNAP' => 'http://s3-eu-west-1.amazonaws.com/build.eu-west-1.elastic.co/origin/$VERSION$/nightly/JDK7/elasticsearch-latest-SNAPSHOT.$EXT$',
-    'URL_TAGS' => 'http://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-$VERSION$.$EXT$'
+    'URL_TAGS' => 'http://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-$VERSION$.$EXT$',
+    'URL_TAGS2' => 'https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/$EXT$/elasticsearch/$VERSION$/elasticsearch-$VERSION$.$EXT$'
   }
 
   es_version = ENV['ES_VERSION']
@@ -18,7 +19,12 @@ def get_url
     url = urls["URL_SNAP"].gsub('$VERSION$', ver)
   else
     # we are testing a released version
-    url = urls["URL_TAGS"].gsub('$VERSION$', es_version)
+    if es_version =~ /^2/
+      _url = urls['URL_TAGS2']
+    else
+      _url = urls['URL_TAGS']
+    end
+    url = _url.gsub('$VERSION$', es_version)
   end
 
   return url
