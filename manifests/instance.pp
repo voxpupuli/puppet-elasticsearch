d9fc3c6a2e580802730f4ad1c90ec5bc6770c0ab
Richard Pijnenburg <richard@ispavailability.com>
2016-01-08 11:45:45 +0100

    Remove recurse option on data dir
    Reported in #532 it slows down the puppet runs and we dont really need
    it

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -211,7 +211,6 @@ define elasticsearch::instance(
       owner   => $elasticsearch::elasticsearch_user,
       group   => undef,
       mode    => '0644',
-      recurse => true,
       require => [ Exec["mkdir_datadir_elasticsearch_${name}"], Class['elasticsearch::package'] ],
       before  => Elasticsearch::Service[$name],
     }
