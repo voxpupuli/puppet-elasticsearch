3d4e7541443c31c08e3e8fed661162ce42b9c650
Tyler Langlois <tjl@byu.net>
2016-04-15 18:22:38 -0400

    WIP: shield esusers provider and type prototype; define draft
    [skip ci]

---
+++ lib/puppet/type/elasticsearch_shield_user.rb
@@ -0,0 +1,36 @@
+Puppet::Type.newtype(:elasticsearch_shield_user) do
+  desc "Type to model Elasticsearch shield users."
+
+  ensurable do
+    defaultvalues
+    defaultto :present
+  end
+
+  newparam(:username, :namevar => true) do
+    desc 'User name.'
+  end
+
+  newparam(:password) do
+    desc 'Plaintext password for user.'
+
+    validate do |value|
+      if value.length < 6
+        raise ArgumentError, 'Password must be at least 6 characters long'
+      end
+    end
+
+    def is_to_s currentvalue
+      return '[old password hash redacted]'
+    end
+    def should_to_s newvalue
+      return '[new password hash redacted]'
+    end
+  end
+
+  newproperty(:roles, :array_matching => :all) do
+    desc 'Array of roles that the user should belong to.'
+    def insync? is
+      is.sort == should.sort
+    end
+  end
+end
