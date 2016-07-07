7a8259a47ed6d8ff03c8e6920a87662ed53cfbb4
Tyler Langlois <tjl@byu.net>
2016-07-07 17:23:01 -0400

    testing: initial update for template acceptance tests

--- spec/acceptance/003_template_spec.rb
+++ spec/acceptance/003_template_spec.rb
@@ -18,48 +18,99 @@ describe 'elasticsearch::template', :with_cleanup do
 
   describe 'valid json template' do
 
-    it 'should run successfully' do
+    context 'from file', :with_cleanup do
 
-      pp = <<-EOS
-        class { 'elasticsearch':
-          config => {
-            'node.name' => 'elasticsearch001',
-            'cluster.name' => '#{test_settings['cluster_name']}'
-          },
-          manage_repo => true,
-          repo_version => '#{test_settings['repo_version']}',
-          java_install => true
-        }
+      it 'should run successfully' do
 
-        elasticsearch::instance { 'es-01':
-          config => {
-            'node.name' => 'elasticsearch001',
-            'http.port' => '#{test_settings['port_a']}'
+        pp = <<-EOS
+          class { 'elasticsearch':
+            config => {
+              'node.name' => 'elasticsearch001',
+              'cluster.name' => '#{test_settings['cluster_name']}'
+            },
+            manage_repo => true,
+            repo_version => '#{test_settings['repo_version']}',
+            java_install => true
           }
-        }
 
-        elasticsearch::template { 'foo':
-          ensure => 'present',
-          file => 'puppet:///modules/another/good.json'
-        }
-      EOS
+          elasticsearch::instance { 'es-01':
+            config => {
+              'node.name' => 'elasticsearch001',
+              'http.port' => '#{test_settings['port_a']}'
+            }
+          }
 
-      # Run it twice and test for idempotency
-      apply_manifest pp, :catch_failures => true
-      apply_manifest pp, :catch_changes => true
+          elasticsearch::template { 'foo':
+            ensure => 'present',
+            file => 'puppet:///modules/another/good.json'
+          }
+        EOS
+
+        # Run it twice and test for idempotency
+        apply_manifest pp, :catch_failures => true
+        apply_manifest pp, :catch_changes => true
+      end
+
+      describe port(test_settings['port_a']) do
+        it 'open', :with_retries do should be_listening end
+      end
+
+      describe server :container do
+        describe http(
+          "http://localhost:#{test_settings['port_a']}/_template/foo",
+          :faraday_middleware => middleware
+        ) do
+          it 'returns the installed template', :with_retries do
+            expect(JSON.parse(response.body)).to have_key('foo')
+          end
+        end
+      end
     end
 
-    describe port(test_settings['port_a']) do
-      it 'open', :with_retries do should be_listening end
-    end
+    describe 'from content' do
+      it 'should run successfully' do
 
-    describe server :container do
-      describe http(
-        "http://localhost:#{test_settings['port_a']}/_template/foo",
-        :faraday_middleware => middleware
-      ) do
-        it 'returns the installed template', :with_retries do
-          expect(JSON.parse(response.body)).to have_key('foo')
+        pp = <<-EOS
+          class { 'elasticsearch':
+            config => {
+              'node.name' => 'elasticsearch001',
+              'cluster.name' => '#{test_settings['cluster_name']}'
+            },
+            manage_repo => true,
+            repo_version => '#{test_settings['repo_version']}',
+            java_install => true
+          }
+
+          elasticsearch::instance { 'es-01':
+            config => {
+              'node.name' => 'elasticsearch001',
+              'http.port' => '#{test_settings['port_a']}'
+            }
+          }
+
+          elasticsearch::template { 'foo':
+            ensure => 'present',
+            content => '#{test_settings['good_json']}'
+          }
+        EOS
+
+        # Run it twice and test for idempotency
+        apply_manifest pp, :catch_failures => true
+        apply_manifest pp, :catch_changes => true
+      end
+
+      describe port(test_settings['port_a']) do
+        it 'open', :with_retries do should be_listening end
+      end
+
+      describe server :container do
+        describe http(
+          "http://localhost:#{test_settings['port_a']}/_template/foo",
+          :faraday_middleware => middleware
+        ) do
+          it 'returns the installed template', :with_retries do
+            expect(JSON.parse(response.body)).to have_key('foo')
+          end
         end
       end
     end
