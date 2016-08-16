26d91d886cf2e86b4afb524d93d53087f3c4de6d
Tyler Langlois <tjl@byu.net>
2016-08-16 17:06:25 -0400

    Refactor plugin_name to library and parser function

---
+++ lib/puppet/parser/functions/es_plugin_name.rb
@@ -0,0 +1,30 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__),"..","..",".."))
+
+require 'puppet_x/elastic/plugin_name'
+
+module Puppet::Parser::Functions
+  newfunction(
+    :es_plugin_name,
+    :type => :rvalue,
+    :doc => <<-'ENDHEREDOC') do |args|
+    Given a string, return the best guess at what the directory name
+    will be for the given plugin. Any arguments past the first will
+    be fallbacks (using the same logic) should the first fail.
+
+    For example, all the following return values are "plug":
+
+        es_plugin_name('plug')
+        es_plugin_name('foo/es-plug/1.3.2')
+    ENDHEREDOC
+
+    if args.length < 1
+      raise Puppet::ParseError,
+        'wrong number of arguments, at least one value required'
+    end
+
+    args.each do |arg|
+      next unless arg.is_a? String
+      return Puppet_X::Elastic::plugin_name arg
+    end
+  end
+end
