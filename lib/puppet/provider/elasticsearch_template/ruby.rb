60a605e250ad328d1c1e2c01aa2ff3a6468f163c
Tyler Langlois <tjl@byu.net>
2016-08-25 14:48:09 -0400

    Better error output and safety when handling rest requests

--- lib/puppet/provider/elasticsearch_template/ruby.rb
+++ lib/puppet/provider/elasticsearch_template/ruby.rb
@@ -32,7 +32,16 @@ Puppet::Type.type(:elasticsearch_template).provide(:ruby) do
     http.open_timeout = timeout
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE if not validate_tls
 
-    http.request req
+    begin
+      http.request req
+    rescue EOFError => e
+      # Because the provider attempts a best guess at API access, we
+      # only fail when HTTP operations fail for mutating methods.
+      unless ['GET', 'OPTIONS', 'HEAD'].include? req.method
+        raise Puppet::Error,
+          "Received '#{e}' from the Elasticsearch API. Are your API settings correct?"
+      end
+    end
   end
 
   def self.templates protocol='http', \
@@ -51,7 +60,7 @@ Puppet::Type.type(:elasticsearch_template).provide(:ruby) do
 
     response = rest http, req, validate_tls, timeout, username, password
 
-    if response.code.to_i == 200
+    if response.respond_to? :code and response.code.to_i == 200
       JSON.parse(response.body).map do |name, template|
         {
           :name => name,
