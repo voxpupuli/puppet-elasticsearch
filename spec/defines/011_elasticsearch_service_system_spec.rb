b31936e6f0e4fd22c4d087c7537bcf9bb33b77c0
Tyler Langlois <tjl@byu.net>
2016-11-08 15:27:53 -0500

    testing: boyscouting on init/service def tests

--- spec/defines/011_elasticsearch_service_system_spec.rb
+++ spec/defines/011_elasticsearch_service_system_spec.rb
@@ -23,7 +23,11 @@ describe 'elasticsearch::service::systemd', :type => 'define' do
           :common => ''
       }) }
       let(:title) { 'es-01' }
-      let(:pre_condition) { 'class {"elasticsearch": config => { "node" => {"name" => "test" }}}' }
+      let(:pre_condition) {%q{
+        class { "elasticsearch":
+          config => { "node" => {"name" => "test" }}
+        }
+      }}
 
       if facts[:operatingsystem] == 'OpenSuSE' and
         facts[:operatingsystemrelease].to_i >= 13
@@ -32,7 +36,7 @@ describe 'elasticsearch::service::systemd', :type => 'define' do
         let(:systemd_service_path) { '/lib/systemd/system' }
       end
 
-      context "Setup service" do
+      context 'setup service' do
 
         let :params do {
           :ensure => 'present',
@@ -40,22 +44,26 @@ describe 'elasticsearch::service::systemd', :type => 'define' do
         } end
 
         it { should contain_elasticsearch__service__systemd('es-01') }
-        it { should contain_exec('systemd_reload_es-01').with(:command => '/bin/systemctl daemon-reload') }
-        it { should contain_service('elasticsearch-instance-es-01').with(:ensure => 'running', :enable => true, :provider => 'systemd') }
+        it { should contain_exec('systemd_reload_es-01')
+          .with(:command => '/bin/systemctl daemon-reload') }
+        it { should contain_service('elasticsearch-instance-es-01')
+          .with(:ensure => 'running', :enable => true, :provider => 'systemd') }
       end
 
-      context "Remove service" do
+      context 'remove service' do
 
         let :params do {
           :ensure => 'absent'
         } end
 
         it { should contain_elasticsearch__service__systemd('es-01') }
-        it { should contain_exec('systemd_reload_es-01').with(:command => '/bin/systemctl daemon-reload') }
-        it { should contain_service('elasticsearch-instance-es-01').with(:ensure => 'stopped', :enable => false, :provider => 'systemd') }
+        it { should contain_exec('systemd_reload_es-01')
+          .with(:command => '/bin/systemctl daemon-reload') }
+        it { should contain_service('elasticsearch-instance-es-01')
+          .with(:ensure => 'stopped', :enable => false, :provider => 'systemd') }
       end
 
-      context "unmanaged" do
+      context 'unmanaged' do
         let :params do {
           :ensure => 'present',
           :status => 'unmanaged'
@@ -63,42 +71,57 @@ describe 'elasticsearch::service::systemd', :type => 'define' do
 
 
         it { should contain_elasticsearch__service__systemd('es-01') }
-        it { should contain_service('elasticsearch-instance-es-01').with(:enable => false) }
+        it { should contain_service('elasticsearch-instance-es-01')
+          .with(:enable => false) }
         it { should contain_augeas('defaults_es-01') }
 
       end
 
-      context "Defaults file" do
+      context 'defaults file' do
 
-        context "Set via file" do
+        context 'set via file' do
           let :params do {
             :ensure => 'present',
-        :status => 'enabled',
-        :init_defaults_file => 'puppet:///path/to/initdefaultsfile'
+            :status => 'enabled',
+            :init_defaults_file => 'puppet:///path/to/initdefaultsfile'
           } end
 
-          it { should contain_file('/etc/sysconfig/elasticsearch-es-01').with(:source => 'puppet:///path/to/initdefaultsfile', :before => 'Service[elasticsearch-instance-es-01]') }
+          it { should contain_file('/etc/sysconfig/elasticsearch-es-01')
+            .with(
+              :source => 'puppet:///path/to/initdefaultsfile',
+              :before => 'Service[elasticsearch-instance-es-01]'
+          ) }
         end
 
-        context "Set via hash" do
+        context 'set via hash' do
           let :params do {
             :ensure        => 'present',
             :status        => 'enabled',
             :init_defaults => {'ES_HOME' => '/usr/share/elasticsearch' }
           } end
 
-          it { should contain_augeas('defaults_es-01').with(:incl => '/etc/sysconfig/elasticsearch-es-01', :changes => "set ES_GROUP 'elasticsearch'\nset ES_HOME '/usr/share/elasticsearch'\nset ES_USER 'elasticsearch'\nset MAX_OPEN_FILES '65535'\n", :before => 'Service[elasticsearch-instance-es-01]') }
+          it { should contain_augeas('defaults_es-01')
+            .with(
+              :incl => '/etc/sysconfig/elasticsearch-es-01',
+              :changes => [
+                "set ES_GROUP 'elasticsearch'",
+                "set ES_HOME '/usr/share/elasticsearch'",
+                "set ES_USER 'elasticsearch'",
+                "set MAX_OPEN_FILES '65535'"
+              ].join("\n") << "\n",
+              :before => 'Service[elasticsearch-instance-es-01]'
+          ) }
         end
 
         context 'restarts when "restart_on_change" is true' do
-          let(:pre_condition) { %q{
+          let(:pre_condition) {%q{
             class { "elasticsearch":
               config => { "node" => {"name" => "test" }},
               restart_on_change => true
             }
           }}
 
-          context "Set via file" do
+          context 'set via file' do
             let :params do {
               :ensure             => 'present',
               :status             => 'enabled',
@@ -129,7 +152,12 @@ describe 'elasticsearch::service::systemd', :type => 'define' do
               'defaults_es-01'
             ).with(
               :incl => '/etc/sysconfig/elasticsearch-es-01',
-              :changes => "set ES_GROUP 'elasticsearch'\nset ES_HOME '/usr/share/elasticsearch'\nset ES_USER 'elasticsearch'\nset MAX_OPEN_FILES '65535'\n",
+              :changes => [
+                "set ES_GROUP 'elasticsearch'",
+                "set ES_HOME '/usr/share/elasticsearch'",
+                "set ES_USER 'elasticsearch'",
+                "set MAX_OPEN_FILES '65535'"
+              ].join("\n") << "\n"
             )}
             it { should contain_augeas(
               'defaults_es-01'
@@ -145,13 +173,13 @@ describe 'elasticsearch::service::systemd', :type => 'define' do
         end
 
         context 'does not restart when "restart_on_change" is false' do
-          let(:pre_condition) { %q{
+          let(:pre_condition) {%q{
             class { "elasticsearch":
               config => { "node" => {"name" => "test" }},
             }
           }}
 
-          context "Set via file" do
+          context 'set via file' do
             let :params do {
               :ensure             => 'present',
               :status             => 'enabled',
@@ -168,7 +196,7 @@ describe 'elasticsearch::service::systemd', :type => 'define' do
         end
       end
 
-      context 'Init file' do
+      context 'init file' do
         let(:pre_condition) {%q{
           class { "elasticsearch":
             config => { "node" => {"name" => "test" }}
@@ -201,7 +229,7 @@ describe 'elasticsearch::service::systemd', :type => 'define' do
         end
 
         context 'restarts when "restart_on_change" is true' do
-          let(:pre_condition) { %q{
+          let(:pre_condition) {%q{
             class { "elasticsearch":
               config => { "node" => {"name" => "test" }},
               restart_on_change => true
@@ -229,7 +257,7 @@ describe 'elasticsearch::service::systemd', :type => 'define' do
         end
 
         context 'does not restart when "restart_on_change" is false' do
-          let(:pre_condition) { %q{
+          let(:pre_condition) {%q{
             class { "elasticsearch":
               config => { "node" => {"name" => "test" }},
             }
