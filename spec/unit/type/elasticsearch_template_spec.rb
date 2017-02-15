af30bb1df879f56f3ca2e5039be344c4fc740158
Tyler Langlois <tjl@byu.net>
2017-02-15 17:32:41 -0500

    testing: finish abstracting rest type tests

--- spec/unit/type/elasticsearch_template_spec.rb
+++ spec/unit/type/elasticsearch_template_spec.rb
@@ -1,184 +1,73 @@
 require 'spec_helper'
 
-describe Puppet::Type.type(:elasticsearch_template) do
+require_relative 'elasticsearch_rest_shared_examples'
 
+# rubocop:disable Metrics/BlockLength
+describe Puppet::Type.type(:elasticsearch_template) do
   let(:resource_name) { 'test_template' }
 
-  describe 'attribute validation' do
-    [
-      :name,
-      :source,
-      :host,
-      :port,
-      :protocol,
-      :validate_tls,
-      :ca_file,
-      :ca_path,
-      :timeout,
-      :username,
-      :password
-    ].each do |param|
-      it "should have a #{param} parameter" do
-        expect(described_class.attrtype(param)).to eq(:param)
-      end
+  include_examples 'REST API types', 'template'
+
+  describe 'template attribute validation' do
+    it 'should have a source parameter' do
+      expect(described_class.attrtype(:source)).to eq(:param)
     end
 
-    [:content, :ensure].each do |prop|
-      it "should have a #{prop} property" do
-        expect(described_class.attrtype(prop)).to eq(:property)
-      end
-    end
-
-    describe 'namevar validation' do
-      it 'should have :name as its namevar' do
-        expect(described_class.key_attributes).to eq([:name])
-      end
+    it 'should have a content property' do
+      expect(described_class.attrtype(:content)).to eq(:property)
     end
 
     describe 'content' do
       it 'should reject non-hash values' do
-        expect { described_class.new(
-          :name => resource_name,
-          :content => '{"foo":}'
-        ) }.to raise_error(Puppet::Error, /hash expected/i)
+        expect do
+          described_class.new(
+            :name => resource_name,
+            :content => '{"foo":}'
+          )
+        end.to raise_error(Puppet::Error, /hash expected/i)
 
-        expect { described_class.new(
-          :name => resource_name,
-          :content => 0
-        ) }.to raise_error(Puppet::Error, /hash expected/i)
+        expect do
+          described_class.new(
+            :name => resource_name,
+            :content => 0
+          )
+        end.to raise_error(Puppet::Error, /hash expected/i)
 
-        expect { described_class.new(
-          :name => resource_name,
-          :content => {}
-        ) }.not_to raise_error
+        expect do
+          described_class.new(
+            :name => resource_name,
+            :content => {}
+          )
+        end.not_to raise_error
       end
 
       it 'should deeply parse PSON-like values' do
         expect(described_class.new(
           :name => resource_name,
-          :content => {'key'=>{'value'=>'0'}}
+          :content => { 'key' => { 'value' => '0' } }
         )[:content]).to include(
-          'key'=>{'value'=>0}
+          'key' => { 'value' => 0 }
         )
-      end
-    end
-
-    describe 'ensure' do
-      it 'should support present as a value for ensure' do
-        expect { described_class.new(
-          :name => resource_name,
-          :ensure => :present,
-          :content => {}
-        ) }.to_not raise_error
-      end
-
-      it 'should support absent as a value for ensure' do
-        expect { described_class.new(
-          :name => resource_name,
-          :ensure => :absent
-        ) }.to_not raise_error
-      end
-
-      it 'should not support other values' do
-        expect { described_class.new(
-          :name => resource_name,
-          :ensure => :foo,
-          :content => {}
-        ) }.to raise_error(Puppet::Error, /Invalid value/)
-      end
-    end
-
-    describe 'host' do
-      it 'should accept IP addresses' do
-        expect { described_class.new(
-          :name => resource_name,
-          :content => {},
-          :host => '127.0.0.1'
-        ) }.not_to raise_error
-      end
-    end
-
-    describe 'port' do
-      [-1, 0, 70000, 'foo'].each do |value|
-        it "should reject invalid port value #{value}" do
-          expect { described_class.new(
-            :name => resource_name,
-            :content => {},
-            :port => value
-          ) }.to raise_error(Puppet::Error, /invalid port/i)
-        end
-      end
-    end
-
-    describe 'validate_tls' do
-      [-1, 0, {}, [], 'foo'].each do |value|
-        it "should reject invalid ssl_verify value #{value}" do
-          expect { described_class.new(
-            :name => resource_name,
-            :content => {},
-            :validate_tls => value
-          ) }.to raise_error(Puppet::Error, /invalid value/i)
-        end
-      end
-
-      [true, false, 'true', 'false', 'yes', 'no'].each do |value|
-        it "should accept validate_tls value #{value}" do
-          expect { described_class.new(
-            :name => resource_name,
-            :content => {},
-            :validate_tls => value
-          ) }.not_to raise_error
-        end
-      end
-    end
-
-    describe 'timeout' do
-      it 'should reject string values' do
-        expect { described_class.new(
-          :name => resource_name,
-          :content => {},
-          :timeout => 'foo'
-        ) }.to raise_error(Puppet::Error, /must be a/)
-      end
-
-      it 'should reject negative integers' do
-        expect { described_class.new(
-          :name => resource_name,
-          :content => {},
-          :timeout => -10
-        ) }.to raise_error(Puppet::Error, /must be a/)
-      end
-
-      it 'should accept integers' do
-        expect { described_class.new(
-          :name => resource_name,
-          :content => {},
-          :timeout => 10
-        ) }.to_not raise_error
-      end
-
-      it 'should accept quoted integers' do
-        expect { described_class.new(
-          :name => resource_name,
-          :content => {},
-          :timeout => '10'
-        ) }.to_not raise_error
       end
     end
 
     describe 'content and source validation' do
       it 'should require either "content" or "source"' do
-        expect { described_class.new(
-          :name => resource_name,
-        ) }.to raise_error(Puppet::Error, /content.*or.*source.*required/)
+        expect do
+          described_class.new(
+            :name => resource_name
+          )
+        end.to raise_error(Puppet::Error, /content.*or.*source.*required/)
       end
 
       it 'should fail with both defined' do
-        expect { described_class.new(
-          :name => resource_name,
-          :content => {},
-          :source => 'puppet:///example.json'
-        ) }.to raise_error(Puppet::Error, /simultaneous/)
+        expect do
+          described_class.new(
+            :name => resource_name,
+            :content => {},
+            :source => 'puppet:///example.json'
+          )
+        end.to raise_error(Puppet::Error, /simultaneous/)
       end
 
       it 'should parse source paths into the content property' do
@@ -210,7 +99,7 @@ describe Puppet::Type.type(:elasticsearch_template) do
             'number_of_replicas' => '2',
             'index' => { 'number_of_shards' => '3' }
           } }
-        )[:content]).to eq({
+        )[:content]).to eq(
           'order' => 0,
           'aliases' => {},
           'mappings' => {},
@@ -220,7 +109,7 @@ describe Puppet::Type.type(:elasticsearch_template) do
               'number_of_shards' => 3
             }
           }
-        })
+        )
       end
 
       it 'detects flat qualified index settings' do
@@ -232,7 +121,7 @@ describe Puppet::Type.type(:elasticsearch_template) do
               'index.number_of_shards' => '3'
             }
           }
-        )[:content]).to eq({
+        )[:content]).to eq(
           'order' => 0,
           'aliases' => {},
           'mappings' => {},
@@ -242,7 +131,7 @@ describe Puppet::Type.type(:elasticsearch_template) do
               'number_of_shards' => 3
             }
           }
-        })
+        )
       end
     end
   end # of describing when validing values
