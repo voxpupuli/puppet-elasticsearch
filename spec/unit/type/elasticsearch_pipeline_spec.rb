21d152ca21f0410ba1100e134cd1c2fb279ea84d
Tyler Langlois <tjl@byu.net>
2017-02-16 14:26:29 -0500

    Standardize on :content for rest provider body

--- spec/unit/type/elasticsearch_pipeline_spec.rb
+++ spec/unit/type/elasticsearch_pipeline_spec.rb
@@ -2,79 +2,8 @@ require 'spec_helper'
 
 require_relative 'elasticsearch_rest_shared_examples'
 
-# rubocop:disable Metrics/BlockLength
 describe Puppet::Type.type(:elasticsearch_pipeline) do
   let(:resource_name) { 'test_pipeline' }
-  let(:default_params) do
-    {}
-  end
 
   include_examples 'REST API types', 'pipeline'
-
-  describe 'pipeline attribute validation' do
-    [
-      :description,
-      :processors
-    ].each do |property|
-      it "should have a #{property} property" do
-        expect(described_class.attrtype(property)).to eq(:property)
-      end
-    end
-
-    describe 'description' do
-      [0, {}].each do |bad_value|
-        it "should reject non-string value #{bad_value}" do
-          expect do
-            described_class.new(
-              :name => resource_name,
-              :description => bad_value
-            )
-          end.to raise_error(Puppet::Error, /string expected/i)
-        end
-      end
-
-      ['', 'desc'].each do |good_value|
-        it "accepts string #{good_value}" do
-          expect do
-            described_class.new(
-              :name => resource_name,
-              :description => good_value
-            )
-          end.not_to raise_error
-        end
-      end
-    end
-
-    describe 'processors' do
-      ['{"foo":}', 0].each do |bad_value|
-        it "should reject non-array value #{bad_value}" do
-          expect do
-            described_class.new(
-              :name => resource_name,
-              :processors => bad_value
-            )
-          end.to raise_error(Puppet::Error, /array expected/i)
-        end
-      end
-
-      [
-        [],
-        [{
-          'set' => {
-            'field' => 'foo',
-            'value' => 'bar'
-          }
-        }]
-      ].each do |good_value|
-        it "accepts array #{good_value}" do
-          expect do
-            described_class.new(
-              :name => resource_name,
-              :processors => [good_value]
-            )
-          end.not_to raise_error
-        end
-      end
-    end
-  end # of describing when validing values
-end # of describe Puppet::Type
+end
--- spec/unit/type/elasticsearch_template_spec.rb
+++ spec/unit/type/elasticsearch_template_spec.rb
@@ -5,53 +5,12 @@ require_relative 'elasticsearch_rest_shared_examples'
 # rubocop:disable Metrics/BlockLength
 describe Puppet::Type.type(:elasticsearch_template) do
   let(:resource_name) { 'test_template' }
-  let(:default_params) do
-    { :content => {} }
-  end
 
   include_examples 'REST API types', 'template'
 
   describe 'template attribute validation' do
     it 'should have a source parameter' do
       expect(described_class.attrtype(:source)).to eq(:param)
-    end
-
-    it 'should have a content property' do
-      expect(described_class.attrtype(:content)).to eq(:property)
-    end
-
-    describe 'content' do
-      it 'should reject non-hash values' do
-        expect do
-          described_class.new(
-            :name => resource_name,
-            :content => '{"foo":}'
-          )
-        end.to raise_error(Puppet::Error, /hash expected/i)
-
-        expect do
-          described_class.new(
-            :name => resource_name,
-            :content => 0
-          )
-        end.to raise_error(Puppet::Error, /hash expected/i)
-
-        expect do
-          described_class.new(
-            :name => resource_name,
-            :content => {}
-          )
-        end.not_to raise_error
-      end
-
-      it 'should deeply parse PSON-like values' do
-        expect(described_class.new(
-          :name => resource_name,
-          :content => { 'key' => { 'value' => '0' } }
-        )[:content]).to include(
-          'key' => { 'value' => 0 }
-        )
-      end
     end
 
     describe 'content and source validation' do
