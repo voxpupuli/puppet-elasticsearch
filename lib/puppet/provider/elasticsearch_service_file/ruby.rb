3aec538798d64e85f769e06d21317d7b1efb482b
Richard Hillmann <richie@project0.de>
2017-05-10 10:46:01 +0200

    fix $LOAD_PATH for puppet_x library, point to relative lib path

--- lib/puppet/provider/elasticsearch_service_file/ruby.rb
+++ lib/puppet/provider/elasticsearch_service_file/ruby.rb
@@ -1,4 +1,4 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__),"..","..","..",".."))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', '..', '..'))
 
 require 'pathname'
 require 'puppet/util/filetype'
--- lib/puppet/type/elasticsearch_template.rb
+++ lib/puppet/type/elasticsearch_template.rb
@@ -1,4 +1,4 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', '..', '..'))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', '..'))
 
 require 'puppet/file_serving/content'
 require 'puppet/file_serving/metadata'
