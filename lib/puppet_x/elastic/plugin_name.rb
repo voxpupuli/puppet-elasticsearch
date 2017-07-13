2aac1ebf22ccfa8cd057d2e579fe116235bc14de
Tyler Langlois <tjl@byu.net>
2017-07-13 15:17:51 -0400

    lib: tests and fix for managing maven-style plugin names

--- lib/puppet_x/elastic/plugin_name.rb
+++ lib/puppet_x/elastic/plugin_name.rb
@@ -2,15 +2,15 @@ module Puppet_X
   module Elastic
     # Attempt to guess at the plugin's final directory name
     def self.plugin_name(original_string)
-      vendor, plugin, _version = original_string.split('/')
-
-      if plugin.nil?
-        # Not delineated by slashes; single plugin name in the style of
-        # commercial plugins post-2.x
-        vendor
-      else # strip off potential es prefixes and return the plugin name
-        plugin.gsub(/(elasticsearch-|es-)/, '')
+      # Try both colon (maven) and slash-delimited (github/elastic.co) names
+      %w[/ :].each do |delimiter|
+        _vendor, plugin, _version = original_string.split(delimiter)
+        # If the string successfully split, assume we found the right format
+        return plugin.gsub(/(elasticsearch-|es-)/, '') unless plugin.nil?
       end
+
+      # Fallback to the originally passed plugin name
+      original_string
     end
   end # of Elastic
 end # of Puppet_X
