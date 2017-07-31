eb4a96555088c92b18b60739363d3b6f2f7ce01d
Tyler Langlois <tjl@byu.net>
2017-07-31 15:33:38 -0400

    testing: fix silly naming cop for 5.x helper

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -198,7 +198,7 @@ end
 require_relative 'spec_acceptance_common'
 
 # Java 8 is only easy to manage on recent distros
-def is_5x_capable?
+def v5x_capable?
   (fact('osfamily') == 'RedHat' and \
         not (fact('operatingsystem') == 'OracleLinux' and \
          fact('operatingsystemmajrelease') == '6')) or \
