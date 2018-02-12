e8a9375b1a1e57fe50be4920ee4620d52ff1d90a
Tyler Langlois <tjl@byu.net>
2018-02-12 15:38:23 -0700

    rewrite plugin provider for more robust version/presence checking

--- lib/puppet_x/elastic/plugin_parsing.rb
+++ lib/puppet_x/elastic/plugin_parsing.rb
@@ -1,11 +1,17 @@
+class ElasticPluginParseFailure < StandardError; end
+
 module Puppet_X
   module Elastic
     def self.plugin_name(raw_name)
       plugin_split(raw_name, 1)
     end
 
+    def self.plugin_version(raw_name)
+      plugin_split(raw_name, 2, false)
+    end
+
     # Attempt to guess at the plugin's final directory name
-    def self.plugin_split(original_string, position)
+    def self.plugin_split(original_string, position, soft_fail = true)
       # Try both colon (maven) and slash-delimited (github/elastic.co) names
       %w[/ :].each do |delimiter|
         parts = original_string.split(delimiter)
@@ -13,7 +19,11 @@ module Puppet_X
         return parts[position].gsub(/(elasticsearch-|es-)/, '') unless parts[position].nil?
       end
 
-      # Fallback to the originally passed plugin name
+      raise(
+        ElasticPluginParseFailure,
+        "could not find element '#{position}' in #{original_string}"
+      ) unless soft_fail
+
       original_string
     end
   end # of Elastic
