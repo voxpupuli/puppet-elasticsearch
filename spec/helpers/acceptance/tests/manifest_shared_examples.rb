e2abce5afdf8185f9ead782d31365ea373ac72ce
Tyler Langlois <tjl@byu.net>
2018-03-21 16:49:52 -0600

    Refactor datapath tests into general form

--- spec/helpers/acceptance/tests/manifest_shared_examples.rb
+++ spec/helpers/acceptance/tests/manifest_shared_examples.rb
@@ -1,16 +1,28 @@
 shared_examples 'manifest application' do |instances, extra_manifest = ''|
   context "#{instances.count}-node manifest" do
     let(:applied_manifest) do
-      manifest + instances.map do |instance, meta|
+      instance_manifest = instances.map do |instance, meta|
         config = meta.map { |k, v| "'#{k}' => '#{v}'," }.join(' ')
         <<-MANIFEST
           elasticsearch::instance { '#{instance}':
             config => {
               #{config}
-            }
+            },
+            #{defined?(manifest_instance_parameters) && manifest_instance_parameters}
           }
         MANIFEST
-      end.join("\n") + extra_manifest
+      end.join("\n")
+
+      <<-MANIFEST
+        class { 'elasticsearch' :
+          #{manifest}
+          #{defined?(manifest_class_parameters) && manifest_class_parameters}
+        }
+
+        #{instance_manifest}
+
+        #{extra_manifest}
+      MANIFEST
     end
 
     it 'applies cleanly' do
