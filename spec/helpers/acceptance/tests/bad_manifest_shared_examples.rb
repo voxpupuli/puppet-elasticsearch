f00bb0a4eb3fc772b379540d0bf19c740ceda661
Tyler Langlois <tjl@byu.net>
2018-04-03 16:13:06 -0600

    Fix regression with bad manifest shared examples

--- spec/helpers/acceptance/tests/bad_manifest_shared_examples.rb
+++ spec/helpers/acceptance/tests/bad_manifest_shared_examples.rb
@@ -1,16 +1,29 @@
 shared_examples 'invalid manifest application' do |instances|
   context "bad #{instances.count}-node manifest" do
     let(:applied_manifest) do
-      manifest + instances.map do |instance, meta|
-        config = meta.map { |k, v| "'#{k}' => '#{v}'," }.join("\n")
+      instance_manifest = instances.map do |instance, meta|
+        config = meta.map { |k, v| "'#{k}' => '#{v}'," }.join(' ')
         <<-MANIFEST
           elasticsearch::instance { '#{instance}':
+            ensure => #{meta.empty? ? 'absent' : 'present'},
             config => {
               #{config}
-            }
+            },
+            #{defined?(manifest_instance_parameters) && manifest_instance_parameters}
           }
         MANIFEST
-      end.join("\n") + extra_manifest
+      end.join("\n")
+
+      <<-MANIFEST
+        class { 'elasticsearch' :
+          #{manifest}
+          #{defined?(manifest_class_parameters) && manifest_class_parameters}
+        }
+
+        #{defined?(skip_instance_manifests) || instance_manifest}
+
+        #{defined?(extra_manifest) && extra_manifest}
+      MANIFEST
     end
 
     it 'fails to apply' do
