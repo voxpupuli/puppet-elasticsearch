bcdc5e9829b8dbd8058f976bd9181cf5adcd8392
Tyler Langlois <tjl@byu.net>
2018-05-30 15:53:39 -0600

    Fix misnamed oss xpack provider defaults file location

--- lib/puppet/provider/elasticsearch_role_mapping/oss_xpack.rb
+++ lib/puppet/provider/elasticsearch_role_mapping/oss_xpack.rb
@@ -7,6 +7,6 @@ Puppet::Type.type(:elasticsearch_role_mapping).provide(
 ) do
   desc 'Provider for OSS X-Pack role mappings.'
 
-  xpack_config 'role_mapping.yml'
+  oss_xpack_config 'role_mapping.yml'
   confine :exists => default_target
 end
