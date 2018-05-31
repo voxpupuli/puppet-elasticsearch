de787a6c01c97aafd404a93d8c2cea1a84331036
Tyler Langlois <tjl@byu.net>
2018-05-31 13:19:18 -0600

    Recursively manage config dir instead of copying individual files

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -349,14 +349,40 @@ define elasticsearch::instance (
       require => Class['elasticsearch::package'],
       before  => Elasticsearch::Service[$name],
     }
-
-    file { $configdir:
+    -> file { $configdir:
       ensure  => 'directory',
-      mode    => '0755',
+      # Copy files from the stock configuration directory _into_ the instance
+      # configuration directory. This lets us pull in miscellaneous files that
+      # utilities may create (like X-Pack user/role files) into instance
+      # directories without explicitly naming them, since we can't predict all the
+      # files that plugins may create/manage.
+      #
+      # Special care is needed to avoid copying in _some_ directories/files to
+      # avoid overwriting instance-specific configuration files or other instance
+      # directories.
+      ignore  => [
+        "${elasticsearch::configdir}/elasticsearch.yml",
+        "${elasticsearch::configdir}/jvm.options",
+        "${elasticsearch::configdir}/logging.yml",
+        "${elasticsearch::configdir}/log4j2.properties",
+      ],
+      recurse => 'remote',
+      source  => $elasticsearch::configdir,
       purge   => $elasticsearch::purge_configdir,
       force   => $elasticsearch::purge_configdir,
-      require => [ Exec["mkdir_configdir_elasticsearch_${name}"], Class['elasticsearch::package'] ],
+      tag     => [
+        'elasticsearch_instance_configdir',
+      ],
+      require => Class['elasticsearch::package'],
       before  => Elasticsearch::Service[$name],
+      notify  => $notify_service,
+    }
+
+    # Do _not_ copy in instance directories. This avoids a) recursing
+    # indefinitely by copying our own instance directory and b) copying in any
+    # other potential instance directories.
+    File <| tag == 'elasticsearch_instance_configdir' |> {
+      ignore +> $name
     }
 
     file { "${configdir}/jvm.options":
@@ -384,30 +410,6 @@ define elasticsearch::instance (
         notify  => $notify_service,
         require => Class['elasticsearch::package'],
         before  => Elasticsearch::Service[$name];
-    }
-
-    file { "${configdir}/scripts":
-      ensure  => 'directory',
-      source  => "${elasticsearch::homedir}/scripts",
-      mode    => '0755',
-      recurse => 'remote',
-      owner   => $elasticsearch::elasticsearch_user,
-      group   => $elasticsearch::elasticsearch_group,
-      before  => Elasticsearch::Service[$name],
-      notify  => $notify_service,
-    }
-
-    if $security_plugin != undef {
-      file { "${configdir}/${security_plugin}":
-        ensure  => 'directory',
-        mode    => '0755',
-        source  => "${elasticsearch::configdir}/${security_plugin}",
-        recurse => 'remote',
-        owner   => 'root',
-        group   => '0',
-        before  => Elasticsearch::Service[$name],
-        notify  => $notify_service,
-      }
     }
 
     if $system_key != undef {
