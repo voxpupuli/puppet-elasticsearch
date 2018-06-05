4471b296312853703019449022f24e4f55c352ef
Tyler Langlois <tjl@byu.net>
2018-06-05 12:06:52 -0600

    Update spec tests to reflect new file ownership permissions

--- spec/classes/000_elasticsearch_init_spec.rb
+++ spec/classes/000_elasticsearch_init_spec.rb
@@ -379,7 +379,7 @@ describe 'elasticsearch', :type => 'class' do
         end
 
         it { should contain_file('/etc/elasticsearch')
-          .with(:owner => 'myesuser', :group => 'myesgroup') }
+          .with(:owner => 'root', :group => 'myesgroup') }
         it { should contain_file('/var/log/elasticsearch')
           .with(:owner => 'myesuser') }
         it { should contain_file('/usr/share/elasticsearch')
