8a6fedf6b0028c2832442925acad45e8107f0082
Bradley Koby <kobyb@miamioh.edu>
2018-11-05 11:19:23 -0500

    Add transport port fact

--- lib/facter/es_facts.rb
+++ lib/facter/es_facts.rb
@@ -22,15 +22,15 @@ module EsFacts
     tls_keys.any? { |key| config.key? key and config[key] == true }
   end
 
-  # Helper to determine the instance port number
-  def self.get_port(config)
+  # Helper to determine the instance http.port number
+  def self.get_httpport(config)
     enabled = 'http.enabled'
-    port = 'http.port'
+    httpport = 'http.port'
 
     if not config[enabled].nil? and config[enabled] == 'false'
       false
-    elsif not config[port].nil?
-      { config[port] => ssl?(config) }
+    elsif not config[httpport].nil?
+      { config[httpport] => ssl?(config) }
     else
       { '9200' => ssl?(config) }
     end
@@ -44,8 +44,13 @@ module EsFacts
   # rubocop:disable Metrics/PerceivedComplexity
   def self.run
     dir_prefix = '/etc/elasticsearch'
-    # Ports is a hash of port_number => ssl?
-    ports = {}
+    # httpports is a hash of port_number => ssl?
+    httpports = {}
+    transportports = []
+    http_bound_addresses = []
+    transport_bound_addresses = []
+    transport_publish_addresses = []
+    nodes = {}
 
     # only when the directory exists we need to process the stuff
     return unless File.directory?(dir_prefix)
@@ -55,22 +60,24 @@ module EsFacts
 
       if File.readable?("#{dir_prefix}/#{dir}/elasticsearch.yml")
         config_data = YAML.load_file("#{dir_prefix}/#{dir}/elasticsearch.yml")
-        port = get_port(config_data)
-        next unless port
-        ports.merge! port
+        httpport = get_httpport(config_data)
+        if httpport
+          httpports.merge! httpport
+        end
       end
     end
 
     begin
-      if ports.keys.count > 0
+      if httpports.keys.count > 0
 
-        add_fact('elasticsearch', 'ports', ports.keys.join(','))
-        ports.each_pair do |port, ssl|
+        add_fact('elasticsearch', 'ports', httpports.keys.join(','))
+
+        httpports.each_pair do |httpport, ssl|
           next if ssl
 
-          key_prefix = "elasticsearch_#{port}"
+          key_prefix = "elasticsearch_#{httpport}"
 
-          uri = URI("http://localhost:#{port}")
+          uri = URI("http://localhost:#{httpport}")
           http = Net::HTTP.new(uri.host, uri.port)
           http.read_timeout = 10
           http.open_timeout = 2
@@ -81,7 +88,7 @@ module EsFacts
           add_fact(key_prefix, 'name', json_data['name'])
           add_fact(key_prefix, 'version', json_data['version']['number'])
 
-          uri2 = URI("http://localhost:#{port}/_nodes/#{json_data['name']}")
+          uri2 = URI("http://localhost:#{httpport}/_nodes/#{json_data['name']}")
           http2 = Net::HTTP.new(uri2.host, uri2.port)
           http2.read_timeout = 10
           http2.open_timeout = 2
@@ -110,13 +117,34 @@ module EsFacts
             end
           end
           add_fact(key_prefix, 'plugins', plugin_names.join(','))
+
+          nodes_data['http']['bound_address'].each {|i| http_bound_addresses << i }
+          nodes_data['transport']['bound_address'].each {|i| transport_bound_addresses << i }
+          transport_publish_addresses << nodes_data['transport']['publish_address']
+          transportports << nodes_data['settings']['transport']['tcp']['port']
+
+          node = { 'http_ports' => httpports.keys,
+                   'transport_ports' => transportports,
+                   'http_bound_addresses' => http_bound_addresses,
+                   'transport_bound_addresses' => transport_bound_addresses,
+                   'transport_publish_addresses' => transport_publish_addresses,
+                   json_data['name'] => { 'settings' => nodes_data['settings'], 'http' => nodes_data['http'], 'transport' => nodes_data['transport'] } }
+          nodes.merge! node
+
         end
       end
     rescue
     end
+    Facter.add(:elasticsearch) do
+      setcode do
+        nodes
+      end
+      nodes unless nodes.empty?
+    end
   end
   # rubocop:enable Metrics/CyclomaticComplexity
   # rubocop:enable Metrics/PerceivedComplexity
+
 end
 
 EsFacts.run
