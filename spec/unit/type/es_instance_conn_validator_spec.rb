a5ac7374f806c4c00edacce8bc8dfa18198f584f
Gavin Williams <gavin.williams@elastic.co>
2020-05-04 14:58:12 +0100

    Fix rubocop warnings on new unit test file

--- spec/unit/type/es_instance_conn_validator_spec.rb
+++ spec/unit/type/es_instance_conn_validator_spec.rb
@@ -4,18 +4,18 @@ describe Puppet::Type.type(:es_instance_conn_validator) do
   let(:resource_name) { 'conn-validator' }
   let(:conn_validator) do
     Puppet::Type.type(:es_instance_conn_validator)
-      .new(name: resource_name)
+                .new(name: resource_name)
   end
 
   describe 'when validating attributes' do
     [:name, :server, :port, :timeout, :sleep_interval].each do |param|
-      it "should have a #{param} parameter" do
+      it 'should have a #{param} parameter' do
         expect(described_class.attrtype(param)).to eq(:param)
       end
     end
 
     [:ensure].each do |prop|
-      it "should have a #{prop} property" do
+      it 'should have a #{prop} property' do
         expect(described_class.attrtype(prop)).to eq(:property)
       end
     end
@@ -50,18 +50,18 @@ describe Puppet::Type.type(:es_instance_conn_validator) do
         ) }.to raise_error(Puppet::Error, /Invalid value/)
       end
     end # describe 'ensure'
-  
+
     describe 'timeout' do
-      it "should support a numerical value" do
+      it 'should support a numerical value' do
         conn_validator[:timeout] = 120
         expect(conn_validator[:timeout]).to eq(120)
       end
 
-      it "should have a default value of 60" do
+      it 'should have a default value of 60' do
         expect(conn_validator[:timeout]).to eq(60)
       end
 
-      it "should not support a non-numeric value" do
+      it 'should not support a non-numeric value' do
         expect do
           conn_validator[:timeout] = 'string'
         end.to raise_error(Puppet::Error, /invalid value/)
@@ -69,16 +69,16 @@ describe Puppet::Type.type(:es_instance_conn_validator) do
     end # describe 'timeout'
 
     describe 'sleep_interval' do
-      it "should support a numerical value" do
+      it 'should support a numerical value' do
         conn_validator[:sleep_interval] = 120
         expect(conn_validator[:sleep_interval]).to eq(120)
       end
 
-      it "should have a default value of 10" do
+      it 'should have a default value of 10' do
         expect(conn_validator[:sleep_interval]).to eq(10)
       end
 
-      it "should not support a non-numeric value" do
+      it 'should not support a non-numeric value' do
         expect do
           conn_validator[:sleep_interval] = 'string'
         end.to raise_error(Puppet::Error, /invalid value/)
